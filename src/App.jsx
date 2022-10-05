864702bfef820fc48ced5954bd5c3c2ce9d7d054
Julian Riedinger <julianriedinger27@gmail.com>
2022-10-05 17:23:49 -0300

    fixed modal

--- src/App.jsx
+++ src/App.jsx
@@ -36,37 +36,38 @@ function App() {
 
 	return (
 		<main className="text-lg min-h-screen flex flex-col justify-center items-center font-alkalami relative">
-			<div className="p-10 shadow-2xl lg:w-[500px]">
-				<h1 className="text-4xl mb-5">Regalos:</h1>
-				<button
-					className="px-4 py-2 mb-5 bg-red-500 text-white uppercase tracking-widest rounded-lg shadow-lg"
-					onClick={changeModalOpen}
-				>
-					Agregar Regalo
-				</button>
-				{modalOpen && (
-					<GiftForm
-						gifts={gifts}
-						addGift={addGift}
-						changeModalOpen={changeModalOpen}
-					/>
-				)}
-				{gifts.length > 0 ? (
-					<GiftList gifts={gifts} handleDelete={handleDelete} />
-				) : (
-					<h2 className="text-teal-500 text-center">
-						Aqui se veran tus regalos...
-					</h2>
-				)}
-				{gifts.length > 0 && (
+			{modalOpen ? (
+				<GiftForm
+					gifts={gifts}
+					addGift={addGift}
+					changeModalOpen={changeModalOpen}
+				/>
+			) : (
+				<div className="p-10 shadow-2xl lg:w-[500px]">
+					<h1 className="text-4xl mb-5">Regalos:</h1>
 					<button
-						className="px-4 py-2 bg-teal-500 text-white uppercase tracking-widest rounded-lg shadow-lg mt-5 w-64 block mx-auto"
-						onClick={clear}
+						className="px-4 py-2 mb-5 bg-red-500 text-white uppercase tracking-widest rounded-lg shadow-lg"
+						onClick={changeModalOpen}
 					>
-						Borrar Todo
+						Agregar Regalo
 					</button>
-				)}
-			</div>
+					{gifts.length > 0 ? (
+						<GiftList gifts={gifts} handleDelete={handleDelete} />
+					) : (
+						<h2 className="text-teal-500 text-center">
+							Aqui se veran tus regalos...
+						</h2>
+					)}
+					{gifts.length > 0 && (
+						<button
+							className="px-4 py-2 bg-teal-500 text-white uppercase tracking-widest rounded-lg shadow-lg mt-5 w-64 block mx-auto"
+							onClick={clear}
+						>
+							Borrar Todo
+						</button>
+					)}
+				</div>
+			)}
 		</main>
 	);
 }
--- src/components/GiftForm.jsx
+++ src/components/GiftForm.jsx
@@ -35,67 +35,65 @@ const GiftForm = ({ gifts, addGift, changeModalOpen }) => {
 	};
 
 	return (
-		<>
-			<form
-				className=" flex flex-col p-10 flex-wrap gap-5 absolute min-h-[60vh] w-[320px] right-10 bottom-44 lg:left-96 lg:w-[700px] bg-white rounded-xl shadowx-xl"
-				onSubmit={handleSubmit}
-			>
-				<label htmlFor="regalo">Completa la Info de tu regalo</label>
-				<input
-					type="text"
-					className="p-2 shadow-lg rounded-lg w-full"
-					placeholder="Regalo..."
-					id="regalo"
-					name="nombre"
-					value={giftInfo.nombre}
-					onChange={handleChange}
-				/>
-				<input
-					type="text"
-					className="p-2 shadow-lg rounded-lg w-full"
-					placeholder="Destinatario..."
-					name="to"
-					onChange={handleChange}
-					value={giftInfo.to}
-				/>
-				<input
-					type="number"
-					className=" p-2 shadow-lg rounded-lg w-14"
-					value={giftInfo.quantity}
-					name="quantity"
-					min={1}
-					max={100}
-					onChange={handleChange}
-				/>
-				<input
-					type="search"
-					className=" p-2 shadow-lg rounded-lg w-full"
-					placeholder="https://image.png"
-					value={giftInfo.image}
-					name="image"
-					onChange={handleChange}
-					required
-				/>
+		<form
+			className=" flex flex-col p-10 flex-wrap w-11/12 gap-5 lg:w-[700px] bg-white rounded-xl shadowx-xl"
+			onSubmit={handleSubmit}
+		>
+			<label htmlFor="regalo">Completa la Info de tu regalo</label>
+			<input
+				type="text"
+				className="p-2 shadow-lg rounded-lg w-full"
+				placeholder="Regalo..."
+				id="regalo"
+				name="nombre"
+				value={giftInfo.nombre}
+				onChange={handleChange}
+			/>
+			<input
+				type="text"
+				className="p-2 shadow-lg rounded-lg w-full"
+				placeholder="Destinatario..."
+				name="to"
+				onChange={handleChange}
+				value={giftInfo.to}
+			/>
+			<input
+				type="number"
+				className=" p-2 shadow-lg rounded-lg w-14"
+				value={giftInfo.quantity}
+				name="quantity"
+				min={1}
+				max={100}
+				onChange={handleChange}
+			/>
+			<input
+				type="search"
+				className=" p-2 shadow-lg rounded-lg w-full"
+				placeholder="https://image.png"
+				value={giftInfo.image}
+				name="image"
+				onChange={handleChange}
+				required
+			/>
 
-				<div className="flex gap-5 justify-between flex-wrap">
-					<button
-						type="button"
-						className="bg-slate-600 text-white uppercase tracking-widest  rounded-lg shadow-lg px-4 py-2"
-						onClick={changeModalOpen}
-					>
-						Cerrar
-					</button>
+			<div className="flex gap-5 justify-between flex-wrap">
+				<button
+					type="button"
+					className="bg-slate-600 text-white uppercase tracking-widest  rounded-lg shadow-lg px-4 py-2"
+					onClick={changeModalOpen}
+				>
+					Cerrar
+				</button>
 
-					<button
-						type="submit"
-						className="px-4 py-2 bg-red-500 text-white uppercase tracking-widest rounded-lg shadow-lg"
-					>
-						Guardar
-					</button>
-				</div>
-				{error && <h2 className="text-red-500">{error}</h2>}
-			</form>
-		</>
+				<button
+					type="submit"
+					className="px-4 py-2 bg-red-500 text-white uppercase tracking-widest rounded-lg shadow-lg"
+				>
+					Guardar
+				</button>
+			</div>
+			{error && <h2 className="text-red-500">{error}</h2>}
+		</form>
 	);
 };
 
